ac70e9b30e90188207692654762e7877ae94cc8f
Raphaël Balet <raphael.balet@outlook.com>
2025-05-18 22:18:47 +0200

    perf: remove unused

--- projects/ngx-color-scheme/src/lib/isNil.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any */
-export function isNil(value: any): value is null | undefined {
-  return value === null || value === undefined
-}
